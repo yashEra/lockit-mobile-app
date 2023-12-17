416a440a9de70e2642414a46c4b829b20d8948db
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-17 15:34:54 +0530

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.lockit_mobile_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.lockit_mobile_app"
