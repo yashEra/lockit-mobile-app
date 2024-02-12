5a30548fa7e536d2eeadea041f6b4e6388303569
pradisharoon <k.pradeeshan4@gmail.com>
2024-02-12 18:33:21 +0530

    bugs are fixed

---
+++ app/src/main/java/com/example/lockit_mobile_app/Login.kt
@@ -0,0 +1,147 @@
+package com.example.lockit_mobile_app
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okio.IOException
+
+
+class Login : AppCompatActivity() {
+
+    private lateinit var client: OkHttpClient
+
+    private lateinit var createAccount: Button
+
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+
+
+    private  lateinit var buttonSignIn: Button
+
+    private lateinit var loading: ProgressBar
+
+    private val getURL: String = "https://reqres.in/api/users"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        editTextUsername = findViewById(R.id.username)
+        editTextPassword = findViewById(R.id.password)
+
+        buttonSignIn = findViewById(R.id.signin)
+
+        createAccount = findViewById(R.id.createAccount)
+
+        loading = findViewById(R.id.progressBar)
+
+        createAccount.setOnClickListener{
+            val intent = Intent(applicationContext,SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        client = OkHttpClient()
+
+        buttonSignIn.setOnClickListener{
+            loading.visibility = View.VISIBLE
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+
+            if (!isNetworkAvailable()) {
+                Toast.makeText(this@Login, "No internet connection", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (username.isEmpty()) {
+                Toast.makeText(this@Login, "Username can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this@Login, "Password can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            signup(username, password)
+        }
+
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                // Network is available
+//                Toast.makeText(this@Login, "Online", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLost(network: Network) {
+                // Network is lost
+                Toast.makeText(this@Login, "Offline", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder().build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val isConnected = capabilities != null
+
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+
+        return isConnected
+    }
+
+    private fun signup(username: String, password: String) {
+        val requestBody = FormBody.Builder()
+            .add("username", username)
+            .add("password", password)
+            .build()
+
+        val request = Request.Builder().url(getURL).post(requestBody).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    loading.visibility = View.GONE
+                    Toast.makeText(this@Login, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    loading.visibility = View.GONE
+                    try {
+                        Toast.makeText(this@Login, response.body?.string(), Toast.LENGTH_SHORT).show()
+                        val intent = Intent(applicationContext,DashBoard::class.java)
+                        startActivity(intent)
+                        finish()
+                    } catch (e: IOException) {
+                        throw RuntimeException(e)
+                    }
+                }
+            }
+        })
+    }
+
+}
--- app/src/main/java/com/example/lockit_mobile_app/SignUp.kt
+++ app/src/main/java/com/example/lockit_mobile_app/SignUp.kt
@@ -1,27 +1,182 @@
 package com.example.lockit_mobile_app
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okio.IOException
 
 class SignUp : AppCompatActivity()  {
+
+    private lateinit var client: OkHttpClient
+
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPhoneNumber: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextRetypePassword: EditText
+
+    private lateinit var buttonSignup: Button
+
+    private lateinit var loading: ProgressBar
+
+    private lateinit var login: TextView
+
+//    private val postURL: String = "http://10.0.2.2:5001/lockit-332b1/us-central1/app/register"
+    private val getURL: String = "https://reqres.in/api/users"
+
+
     @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
 
-        val b1: Button = findViewById(R.id.createacc)
-        b1.setOnClickListener {
-            val i = Intent(this@SignUp, MainActivity::class.java)
-            startActivity(i)
+        client = OkHttpClient()
+
+        editTextUsername = findViewById(R.id.username)
+        editTextEmail = findViewById(R.id.email)
+        editTextPhoneNumber = findViewById(R.id.phonenumber)
+        editTextPassword = findViewById(R.id.password)
+        editTextRetypePassword = findViewById(R.id.retypepassword)
+
+        buttonSignup = findViewById(R.id.signup)
+
+        loading = findViewById(R.id.progressBar)
+
+        login = findViewById(R.id.login)
+
+        login.setOnClickListener{
+            val intent = Intent(applicationContext,Login::class.java)
+            startActivity(intent)
+            finish()
         }
 
-        val b2: Button = findViewById(R.id.login)
-        b2.setOnClickListener {
-            val i = Intent(this@SignUp, MainActivity::class.java)
-            startActivity(i)
+        buttonSignup.setOnClickListener {
+            loading.visibility = View.VISIBLE
+            val username = editTextUsername.text.toString()
+            val email = editTextEmail.text.toString()
+            val phoneNumber = editTextPhoneNumber.text.toString()
+            val password = editTextPassword.text.toString()
+            val retypePassword = editTextRetypePassword.text.toString()
+
+            if (!isNetworkAvailable()) {
+                Toast.makeText(this@SignUp, "No internet connection", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (username.isEmpty()) {
+                Toast.makeText(this@SignUp, "Username can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty() || !isValidEmail(email)) {
+                Toast.makeText(this@SignUp, "Enter a valid email address", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (phoneNumber.isEmpty()) {
+                Toast.makeText(this@SignUp, "Phone Number can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this@SignUp, "Password can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (retypePassword.isEmpty()) {
+                Toast.makeText(this@SignUp, "Retype password can't be empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password != retypePassword) {
+                Toast.makeText(this@SignUp, "Passwords are not matching", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            signup(username, email, phoneNumber, password)
         }
+
     }
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                // Network is available
+//                Toast.makeText(this@SignUp, "Online", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLost(network: Network) {
+                // Network is lost
+                Toast.makeText(this@SignUp, "Offline", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder().build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val isConnected = capabilities != null
+
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+
+        return isConnected
+    }
+
+
+    private fun isValidEmail(email: String): Boolean {
+        val emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+        return email.matches(emailRegex.toRegex())
+    }
+
+    private fun signup(username: String, email: String, phoneNumber: String, password: String) {
+        val requestBody = FormBody.Builder()
+            .add("username", username)
+            .add("email", email)
+            .add("phoneNumber", phoneNumber)
+            .add("password", password)
+            .build()
+
+        val request = Request.Builder().url(getURL).post(requestBody).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                runOnUiThread {
+                    loading.visibility = View.GONE
+                    Toast.makeText(this@SignUp, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    loading.visibility = View.GONE
+                    try {
+                        Toast.makeText(this@SignUp, response.body?.string(), Toast.LENGTH_SHORT).show()
+                    } catch (e: IOException) {
+                        throw RuntimeException(e)
+                    }
+                }
+            }
+        })
+    }
+
 }
